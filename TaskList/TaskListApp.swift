29c292b9df25f4d556ea6a4aa057201edd80ae09
stjepanstojcevic <stjepanstojcevic@gmail.com>
2023-05-02 22:42:58 +0200

    Initial Commit

---
+++ TaskList/TaskListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskListApp.swift
+//  TaskList
+//
+//  Created by Stjepan Stojčević on 02.05.2023..
+//
+
+import SwiftUI
+
+@main
+struct TaskListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
