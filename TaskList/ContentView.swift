fe60530ebf9539f45fd45d6fee16e27fee17a414
stjepanstojcevic <stjepanstojcevic@gmail.com>
2023-05-28 00:46:07 +0200

    estetika

--- TaskList/ContentView.swift
+++ TaskList/ContentView.swift
@@ -13,32 +13,51 @@ struct ContentView: View {
     @State var newToDo : String = ""
     var searchBar : some View {
         HStack{
+            
             TextField("Enter your task : ",text: self.$newToDo)
+            Image(systemName: "pencil.line")
+                        .foregroundColor(.red)
             Button(action: self.addNewToDo,label: {Text("Add New")})
         }
     }
     func addNewToDo(){
         taskStore.tasks.append(Task(id:String(taskStore.tasks.count+1),toDoItem: newToDo))
         self.newToDo=""
-        print(taskStore.tasks.count)
     }
     
     var body: some View {
-        NavigationView{
-            VStack{
-                searchBar.padding()
-                List{
-                    ForEach(self.taskStore.tasks){
-                        task in
-                        Text(task.toDoItem)
-                    }.onMove(perform: self.move)
-                        .onDelete(perform: self.delete)
-                }.navigationBarTitle("Todays tasks")
-                .navigationBarItems(trailing: EditButton())
-                Button(action: self.removeAll,label: {Text("Clear")})
+    
+            NavigationView{
+                
+                
+                VStack{
+                    searchBar.padding()
+                   
+                    List{
+                        
+                        ForEach(self.taskStore.tasks){
+                            task in
+                            Text(task.toDoItem)
+                        }.onMove(perform: self.move)
+                            .onDelete(perform: self.delete)
+                    }.navigationBarTitle("Today's tasks", displayMode: .large)
+                        .foregroundColor(.red)
+                        .navigationBarItems(trailing: EditButton())
+                        .navigationBarHidden(false)
+                        .foregroundColor(.red)
 
+                    
+                    HStack{
+                        
+                        Image(systemName: "eraser.line.dashed.fill")
+                            .foregroundColor(.red)
+                        Button(action: self.removeAll,label: {Text("Clear all")})
+                        Image(systemName: "eraser.line.dashed.fill")
+                            .foregroundColor(.red)
+                        
+                    }
+                }
             }
-        }
     }
     func move(from source : IndexSet, to destination : Int)
     {
